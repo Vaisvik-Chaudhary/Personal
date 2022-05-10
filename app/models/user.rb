94185b62ddb28c53ae97a1f5c6031506d0d31ec8
Vaisvik-Chaudhary <vaisvik.chaudhary@bacancy.com>
2022-05-10 12:11:27 +0530

    performing association and authetication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,3 @@
-class User < ActiveRecord::Base
-    has_many :articles
-    before_save { self.email = email.downcase }
-    validates :username, presence: true, uniqueness: { case_sensitive: false }, 
-    length: {minimum:3 , maximum: 25}
-VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-   validates :email, presence: true, length: {maximum:150}, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
-
+class User < ApplicationRecord
+    has_many :comments
 end
